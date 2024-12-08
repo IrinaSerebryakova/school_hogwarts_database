af5f6f7888c397a33740297f757755c5981783a3
Iserebryakova <ia.serebr@gmail.com>
2024-12-08 11:51:17 +0300

    init (SQl+database)

---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,45 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.StudentRepository;
+
+import java.util.*;
+
+@Service
+public class StudentService {
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Student createStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).get();
+    }
+
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
+
+    public Student updateStudent(Student student) {
+        return studentRepository.save(student);
+    }
+    public List<Student> findByAge(int age) {
+        return studentRepository.findByAge(age);
+    }
+
+    public List<Student> findByAgeBetween(int minAge, int maxAge) {
+        return studentRepository.findByAgeBetween(minAge, maxAge);
+    }
+
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
+
+}
